f409673a31c622c4ad22903164954e8cd2b319d2
dh0256 <hyun02549@gmail.com>
2026-05-27 17:24:28 +0900

    feat: add product dtos, domain, cont/serve/repo

--- src/main/java/org/fitory/product/controller/ProductController.java
+++ src/main/java/org/fitory/product/controller/ProductController.java
@@ -3,9 +3,10 @@ package org.fitory.product.controller;
 import core.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import mvc.ResponseEntity;
-import mvc.annotation.GetMapping;
-import mvc.annotation.RequestMapping;
+import mvc.annotation.*;
+import org.fitory.product.dto.CreateProductRequest;
 import org.fitory.product.dto.ProductResponse;
+import org.fitory.product.dto.UpdateProductRequest;
 import org.fitory.product.service.ProductService;
 
 import java.util.List;
@@ -18,9 +19,30 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> getProduct() {
-        return ResponseEntity.ok(productService.findAll());
+    public ResponseEntity<List<ProductResponse>> getProduct(@RequestParam Long categoryId,
+                                                            @RequestParam String sort) {
+        return ResponseEntity.ok(productService.findAll(categoryId, sort));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getByProductId(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findById(id));
+    }
 
+    @PostMapping
+    public ResponseEntity<ProductResponse> create(@RequestBody CreateProductRequest request) {
+        return ResponseEntity.created(productService.create(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> update(@PathVariable Long id,
+                                                        @RequestBody UpdateProductRequest request) {
+        return ResponseEntity.ok(productService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent();
+    }
 }
