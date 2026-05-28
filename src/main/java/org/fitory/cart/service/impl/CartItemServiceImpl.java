df95ce000d1fd3aa72ffeaecbaadc4db05f11336
yyubin <hazing120@gmail.com>
2026-05-28 10:56:05 +0900

    chore: add missing `List` import in `CartItemServiceImpl`

--- src/main/java/org/fitory/cart/service/impl/CartItemServiceImpl.java
+++ src/main/java/org/fitory/cart/service/impl/CartItemServiceImpl.java
@@ -14,6 +14,7 @@ import org.fitory.common.dto.PageResponse;
 import org.fitory.product.dto.ProductResponse;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
